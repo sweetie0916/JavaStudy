7d52b485b84a02883106cb079a17f588ebaf6a77
sweetie0916 <dmstjs2720@naver.com>
2023-07-06 16:58:17 +0900

    7월6일 숙제 끝

---
+++ src/ex08class/Homework.java
@@ -0,0 +1,23 @@
+package ex08class;
+
+import ex01start.E02SystemOutPrintln;
+
+class MyClass {
+	String str;
+
+	public MyClass(String str) {
+		super();
+		this.str = str;
+	}
+	public void showInfo() {
+		System.out.println("숙제끝:"+str);
+	}
+}
+public class Homework {
+
+	public static void main(String[] args) {
+		MyClass mc = new MyClass("클래스생성");
+		mc.showInfo();
+	}
+
+}
